5e1cfdcf4b122e865483de8e27679ac53f2e3d7e
Mcpvioy <68005775+mcpivoy@users.noreply.github.com>
2022-06-16 14:52:51 +0800

    Update Server.c

--- Server.c
+++ Server.c
@@ -109,7 +109,7 @@ void *TRD(void *arg){
 		while(!used[ID]);  
 		memset(buf[ID], 0, sizeof(buf[ID]));
 		//n = read(connfd[ID], buf[ID], MAXLINE);
-		n = recvfrom(sockfd, receivemsg, MAXLINE, 0, NULL, NULL);
+		n = recvfrom(socket, receivemsg, MAXLINE, 0, NULL, NULL);
 		if(n <= 0){
 			sprintf(buf[ID], "(%s:%d)离开啦啦啦", inet_ntop(AF_INET, &clientaddr[ID].sin_addr, str, sizeof(str)), ntohs(clientaddr[ID].sin_port)); //可改
 			memset(spemsg[ID], 0, sizeof(spemsg[ID]));
