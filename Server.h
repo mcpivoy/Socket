963bb0554e26b0441e3fe1e9f46b3e8adf8a3b10
Mcpvioy <68005775+mcpivoy@users.noreply.github.com>
2022-06-16 08:44:56 +0800

    Update Server.h

--- Server.h
+++ Server.h
@@ -1,10 +1,10 @@
 #ifndef SERVER_H
 #define SERVER_H
 
-#define MAXLINE 100
-#define MAXNAME 20
+#define MAXLINE 100    //服务器单次发送的最大长度 可改
+#define MAXNAME 20     //用户名最大长度 可改  请同步修改client.h长度 
 #define SERV_PORT 8000
-#define MAXCON 3
+#define MAXCON 3   //最多可连接用户数  client_num = (MAXCON - server_num)  = 2
 
 void *TRD(void *);
 int Process(int);
